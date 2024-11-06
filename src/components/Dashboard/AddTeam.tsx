f0db3eba8043915b7a876e5b3a0a3dec8c9a4c75
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-11-06 20:07:24 +0100

    Update

--- src/components/Dashboard/AddTeam.tsx
+++ src/components/Dashboard/AddTeam.tsx
@@ -185,9 +185,9 @@ const TeamMemberForm: React.FC = () => {
         {/* Submit Button */}
         <button
           type="submit"
-          className="mt-4 w-full rounded bg-blue-600 p-2 text-white"
+          className="mt-4 rounded bg-[#13802a] px-4 py-2 text-white"
         >
-          Submit
+          Add Team Member
         </button>
       </form>
     </>
