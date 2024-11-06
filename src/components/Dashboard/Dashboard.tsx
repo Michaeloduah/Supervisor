9b2ec19cfd53f0fa13c191a17531e9b9d36ebd01
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-11-06 23:26:28 +0100

    fixed responsiveness

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -18,7 +18,7 @@ const Dashboard: React.FC = () => {
         </div>
         <ChatCard />
       </div>
-      <div className="mt-4 grid grid-cols-12 md:grid-cols-2 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
         <div className="">
           <Reports />
         </div>
